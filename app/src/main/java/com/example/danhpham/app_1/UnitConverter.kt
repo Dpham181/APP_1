e6e1d8272835355eded6568186b671009988651a
thomas <tngo0508@gmail.com>
2018-11-01 21:27:37 -0700

    finish implementation for unit temp. converter

--- app/src/main/java/com/example/danhpham/app_1/UnitConverter.kt
+++ app/src/main/java/com/example/danhpham/app_1/UnitConverter.kt
@@ -11,6 +11,7 @@ import com.example.danhpham.app_1.R.id.*
 import kotlinx.android.synthetic.main.activity_unit_converter.*
 import kotlinx.android.synthetic.main.activity_unit_converter.view.*
 import java.lang.NumberFormatException
+import java.util.*
 
 val units = arrayOf(
     "Celsius",
@@ -30,14 +31,17 @@ class UnitConverter : AppCompatActivity() {
         outputListView.adapter = adapter
 
         inputListView.setOnItemClickListener { _, _, position, _ ->
-
             when (units[position]) {
                 "Celsius" -> outputListView.convertFromCelsius(resultTextView, inputEditText, outputListView)
-                "Kelvin" -> outputListView.convertFromKelvin()
-                "Fahrenheit" -> outputListView.convertFromFahrenheit()
+                "Kelvin" -> outputListView.convertFromKelvin(resultTextView, inputEditText, outputListView)
+                "Fahrenheit" -> outputListView.convertFromFahrenheit(resultTextView, inputEditText, outputListView)
             }
         }
     }
+}
+
+private fun TextView.toTwoDec(num: Double?): String {
+    return String.format(Locale.US, "%.2f", num)
 }
 
 private fun ListView.convertFromCelsius(result: TextView?, input: EditText?, outputList: ListView?) {
@@ -55,46 +59,97 @@ private fun ListView.convertFromCelsius(result: TextView?, input: EditText?, out
     }
 }
 
-private fun ListView.convertFromKelvin() {
-
+private fun ListView.convertFromKelvin(result: TextView?, input: EditText?, outputList: ListView?) {
+    outputList!!.setOnItemClickListener { _, _, position, _ ->
+        try {
+            val start = java.lang.Double.valueOf(input!!.text.toString())
+            when (units[position]) {
+                "Celsius" -> result!!.kelvinToCelsius(start, result, input)
+                "Kelvin" -> result!!.text = input.text
+                "Fahrenheit" -> result!!.kelvinToFahrenheit(start, result, input)
+            }
+        } catch (e: NumberFormatException) {
+            return@setOnItemClickListener
+        }
+    }
 }
 
-private fun ListView.convertFromFahrenheit() {
+private fun TextView.kelvinToFahrenheit(initialUnit: Double?, result: TextView?, input: EditText?) {
+    textWatcherManager(
+        result,
+        input,
+        initialUnit,
+        ::convertKToF)
+}
 
+private fun TextView.kelvinToCelsius(initialUnit: Double?, result: TextView?, input: EditText?) {
+    textWatcherManager(
+        result,
+        input,
+        initialUnit,
+        ::convertKToC)
+}
+
+private fun convertKToC(start : Double?, result: TextView?) {
+    result!!.text = result.toTwoDec(start!! - 273.15)
+}
+
+private fun convertKToF(start : Double?, result: TextView?) {
+    result!!.text = result.toTwoDec((start!! - 273.15) * 9/5 + 32)
+}
+
+private fun ListView.convertFromFahrenheit(result: TextView?, input: EditText?, outputList: ListView?) {
+    outputList!!.setOnItemClickListener { _, _, position, _ ->
+        try {
+            val start = java.lang.Double.valueOf(input!!.text.toString())
+            when (units[position]) {
+                "Celsius" -> result!!.fahrenheitToCelsius(start, result, input)
+                "Kelvin" -> result!!.fahrenheitToKelvin(start, result, input)
+                "Fahrenheit" -> result!!.text = input.text
+            }
+        } catch (e: NumberFormatException) {
+            return@setOnItemClickListener
+        }
+    }
+}
+
+private fun TextView.fahrenheitToKelvin(initialUnit: Double?, result: TextView?, input: EditText?) {
+    textWatcherManager(
+        result,
+        input,
+        initialUnit,
+        ::convertFToK)
+}
+
+private fun convertFToK(start : Double?, result: TextView?) {
+    result!!.text = result.toTwoDec((start!! - 32.0) * 5/9 + 273.15)
+}
+
+private fun TextView.fahrenheitToCelsius(initialUnit: Double?, result: TextView?, input: EditText?) {
+    textWatcherManager(
+        result,
+        input,
+        initialUnit,
+        ::convertFtoC)
+}
+
+private fun convertFtoC(start : Double?, result: TextView?) {
+    result!!.text = result.toTwoDec((start!! - 32.0) * 5/9)
 }
 
 private fun TextView.celsiusToKelvin(initialUnit: Double?, result: TextView?, input: EditText?) {
-    val convertedUnit = initialUnit!! + 273.15
-    result!!.text = convertedUnit.toString()
-    val textChangeListener = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
+    textWatcherManager(
+        result,
+        input,
+        initialUnit,
+        ::convertCToK)
+}
 
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            val notEmpty: TextView.() -> Boolean = { text.isNotEmpty() }
-            if (notEmpty(input!!)) {
-                try {
-                    val originalUnit = java.lang.Double.valueOf(input.text.toString())
-                    val convertUnit = originalUnit + 273.15
-                    result.text = convertUnit.toString()
-                } catch (e: NumberFormatException) {
-                    return
-                }
-            }
-        }
-    }
-    input!!.addTextChangedListener(textChangeListener)
-
+private fun convertCToK(start : Double?, result: TextView?) {
+    result!!.text = result.toTwoDec(start!! + 273.15)
 }
 
 private fun TextView.celsiusToFahrenheit(initialUnit: Double?, result: TextView?, input: EditText?) {
-//    val end = (start!! * 9/5) + 32
-//    result!!.text = end.toString()
     textWatcherManager(
         result,
         input,
@@ -103,8 +158,7 @@ private fun TextView.celsiusToFahrenheit(initialUnit: Double?, result: TextView?
 }
 
 private fun convertCToF(start : Double?, result: TextView?) {
-    val end = (start!! * 9/5) + 32
-    result!!.text = end.toString()
+    result!!.text = result.toTwoDec((start!! * 9/5) + 32.0)
 }
 
 private fun textWatcherManager(
